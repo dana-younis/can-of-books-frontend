60e71ea8de13ecd74ebda140a4c12f754a9362b1
dana-younis <younisdana7@gmail.com>
2021-06-02 15:44:26 +0300

    13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,89 +1,60 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Card from 'react-bootstrap/Card';
-import bookForm from './bookForm.js';
+import BookFormModal from './BookFormModal';
 
-
-
-class MyFavoriteBooks extends React.Component {
-
+class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
       showBestBooksComponent: false,
       server: process.env.REACT_APP_SERVER_URL,
-      showFormPage: false,
+      showFormModal: false,
       email: '',
       bookName: '',
       bookDescription: '',
       bookStatus: ''
     }
   }
-  getBooks = async () => {
-    const { user } = this.props.auth0;
-    try {
-
-      const paramsObj = {
-        email: user.email
-      }
-      const books = await axios.get(`${this.state.server}/books`, { params: paramsObj });
-
-      console.log(books);
-      // console.log(typeof books.data[0].books);
 
 
-
-      this.setState({        
-        books: books.data,
-        showBestBooksComponent: true,
-        emeil: user.email
-
-      });
-      console.log('hi',this.state.books);
-    } catch (error) {
-      console.log(error);
-    }
-   
-  }
 
   updateBookName = (e) => this.setState({ bookName: e.target.value });
   updateBookDescription = (e) => this.setState({ bookDescription: e.target.value });
   updatebookStatus = (e) => this.setState({ bookStatus: e.target.value });
 
-  getNewBook = async (e) => {
+  getNewBook = async () => {
     const { user } = this.props.auth0;
-    e.preventDefault();
-    try {
-
-      const bodyData  = {
+    // try {
+      const bodyData = {
         email: user.email,
         bookName: this.state.bookName,
-        bookDescription: this.state.bookName,
+        bookDescription: this.state.bookDescription,
         bookStatus: this.state.bookStatus
-
       }
-      const books = await axios.post(`${this.state.server}/books`, bodyData );
-      console.log(books);
-      this.setState ({
+      const books = await axios.post(`${this.state.server}/books`, bodyData);
+      console.log(books.data);
+      this.setState({
         books: books.data
       });
-console.log(this.state.books);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  
+      this.state.closeForm();
+
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+   }
   deleteBook = async (index) => {
     // console.log(index);
     const { user } = this.props.auth0;
     const newArrayOfBooks = this.state.books.filter((books, idx) => {
       return idx !== index;
     });
-    
+
     console.log(newArrayOfBooks);
     this.setState({
       books: newArrayOfBooks
@@ -96,54 +67,55 @@ console.log(this.state.books);
     await axios.delete(`${this.state.server}/books/${index}`, { params: query });
 
   }
-
-
-  componentDidMount() {
-    this.setState({
-      showBestBooksComponent: true,
-    });
-
-    this.getBooks();
-    console.log(this.getBooks);
+  componentDidMount = async () => {
+    try {
+      let serverURL = await axios.get(`${this.state.server}/books?email=${this.props.auth0.user.email}`);
+      console.log(serverURL);
+      console.log(serverURL.data)
+      this.setState({
+        books: serverURL.data,
+        showBestBooksComponent: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
-
- 
   showForm = () => {
     this.setState({
-      showFormPage: true,
+      showFormModal: true,
     });
-   
+
   }
 
   closeForm = () => {
     this.setState({
-      showFormPage: false,
+      showFormModal: false,
     });
   }
-  
+
 
   render() {
     console.log(this.state.books);
     return (
       <>
 
-        <Jumbotron>
-         
-            <button onClick={this.showForm}>Add Books</button>
-            {this.state.showFormPage &&
-            <>
-            <bookForm
-              getBookName= {this.updateBookName}
-              getBookDescription= {this.updateBookDescription}
-              getBookStatus= {this.updatebookStatus}       
-              ShowForm = {this.state.showFormPage}
-              closeForm = {this.closeForm}
-              getNewBook = {this.getNewBook}
+        {/* <Jumbotron> */}
 
-            />
+          <button onClick={this.showForm}>Add Books</button>
+          {this.state.showFormModal &&
+            <>
+              <BookFormModal
+                getBookName={this.updateBookName}
+                getBookDescription={this.updateBookDescription}
+                getBookStatus={this.updatebookStatus}
+                ShowForm={this.state.showFormModal}
+                closeForm={this.closeForm}
+                getNewBook={this.getNewBook}
+
+              />
             </>
-            }
-          
+          }
+
           {this.state.showBestBooksComponent &&
             <>
 
@@ -155,10 +127,10 @@ console.log(this.state.books);
                         <Card.Title>Name: {data.name}</Card.Title>
                         <Card.Text>Description: {data.description}</Card.Text>
                         <Card.Text>Status: {data.status}</Card.Text>
-                        <button onClick={() => {this.deleteBook(index)}}>Delete</button>                         
+                        <button onClick={() => { this.deleteBook(index) }}>Delete</button>
                       </Card.Body>
                     </Card>
-                
+
 
                   </>
                 );
@@ -167,15 +139,10 @@ console.log(this.state.books);
               }
             </>
           }
-
-
-        </Jumbotron>
+        {/* </Jumbotron> */}
       </>
-
-
-
     )
   }
 }
 
-export default withAuth0(MyFavoriteBooks);
+export default withAuth0(BestBooks);
