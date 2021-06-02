a4d3dd8a7c3018a42150620e145fb7d55ae8dc99
dana-younis <younisdana7@gmail.com>
2021-06-02 09:31:09 +0300

    12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,88 +2,86 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
-import Card from 'react-bootstrap/Card';
-
+// import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel'
 
 
 
 
 
 class MyFavoriteBooks extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        books: [],
-        show: false,
-        server: process.env.REACT_APP_SERVER_URL
-      }
-    }
-  
-    componentDidMount() {
-      this.setState({
-        show: true,
-      });
-  
-      this.getBooks();
-      console.log(this.getBooks);
-    }
-  
-    getBooks = async () => {
-      const { user } = this.props.auth0;
-      try {
-  
-        const obj = {
-          email: user.email
-      
-        }
-        const books = await axios.get(`${this.state.server}/books`, { params: obj });
-        console.log(books);
-        this.setState({
-          books:books.data,
-          show: true
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  
-    render() {
-      console.log(this.state.books);
-      return (
-        <>
-  
-  
-  
-            {this.state.show &&
-           
-              <>
-                {this.state.books.map((data, index) => {
-                  return (
-                    <>
-                      <Card style={{ width: '18rem' }} key={index}>
-                        <Card.Body>
-                          <Card.Title>Name: {data.name}</Card.Title>
-                          <Card.Text>Description: {data.description}</Card.Text>
-                          <Card.Text>Status: {data.status}</Card.Text>                         
-                        </Card.Body>
-                      </Card>
-                    
-                    </>
-                  );
-  
-                })
-                }
-              </>
-            }
-  
-  
-          
-        </>
-  
-  
-  
-      )
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      show: false,
+      server: process.env.REACT_APP_SERVER_URL
     }
   }
-  
-  export default withAuth0(MyFavoriteBooks);
+
+  componentDidMount() {
+    this.setState({
+      show: true,
+    });
+
+    this.getBooks();
+    console.log(this.getBooks);
+  }
+
+  getBooks = async () => {
+    const { user } = this.props.auth0;
+    try {
+
+      const obj = {
+        email: user.email
+
+      }
+      const books = await axios.get(`${this.state.server}/books`, { params: obj });
+      console.log(books);
+      this.setState({
+        books: books.data,
+        show: true
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.show &&
+          <>
+            <Carousel>
+              {this.state.books.map((data, idx) => {
+                return (
+                  <Carousel.Item interval={1000} style={{ width: '18rem'}} key={idx}>
+                    <Carousel.Caption style={{ color: 'black'}}>
+                      <h1>Name: {data.name} </h1>
+                      {console.log(data.name)}
+                      <p>Description: {data.description}</p>
+                      {console.log(data.description)}
+                      <p>Status: {data.status} </p>
+                        {console.log(data.status)}
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                )
+              })
+              }
+            </Carousel>
+          </>
+        }
+          </>
+    )
+  }
+
+
+
+
+
+
+
+}
+
+export default withAuth0(MyFavoriteBooks);
+
